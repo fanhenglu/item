dedb914d47e8bf1845cb8c57b53eaf0b3def31a1
fanhl <981037415@qq.com>
2020-12-02 15:52:56 +0800

    接口代理跨域

---
+++ mall/vue.config.js
@@ -0,0 +1,17 @@
+/**接口代理跨域 */
+/*module.exports = {
+    devServer:{
+        host:'localhost',
+        port:8080,
+        proxy:{
+            '/api':{
+                target:'https://www.baidu.com',
+                changeOrigin:true,
+                pathRewrite:{
+                    '/api':''
+                }
+            }
+        }
+    }
+}
+*/
